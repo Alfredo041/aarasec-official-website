4214d21921cd39f2eadbd999933726064affb22f
Mateh Elismar <matematicoelismar@gmail.com>
2021-01-06 12:06:17 -0400

    fixed route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-console.log(path.resolve("./public/"));
-app.use("/", express.static(path.resolve("./public/")));
+console.log(path.resolve("./docs/"));
+app.use("/", express.static(path.resolve("./docs/")));
 app.get("*", (req, res) => res.redirect("/"));
 app.listen(9000, () => [console.log("server running on port 9000")]);
